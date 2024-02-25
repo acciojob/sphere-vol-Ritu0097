82445c13afad3303fbe570c67200a5d7736a065a
Ritesh Kishor Chaudhari <100477678+Ritu0097@users.noreply.github.com>
2024-02-25 14:44:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 function volume_sphere() {
-    var radius = document.getElementById("radius").value;
-    radius = parseFloat(radius);
-    if (isNaN(radius) || radius < 0){
-        document.getElementById("volume").value = 'NaN';
-    } 
-    else {
-        var volume = (4/3) * Math.PI * Math.pow(radius, 3);
-        volume = volume.toFixed(4);
-        document.getElementById("volume").value = volume;
-    }
-}
-
+    //Write your code here
+	let radius = parseFloat(document.getElementById("radius").value);
+ 
+	if(radius<0) {
+		return false;
+		
+	}
+ 
+	let volume = (4/3)*Math.PI*Math.pow(radius,3);
+	document.getElementById("volume").value = volume.toFixed(4).toString()
+	return false;
+	
+  
+} 
+ 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
